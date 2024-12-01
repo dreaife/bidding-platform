b3716bc2f178f26fa8d26b88a635b44b85d1306c
dreaife <877261793@qq.com>
2024-12-02 00:51:56 +0900

    update / page

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World!\nGet started at https://github.com/dreaife/bidding-platform\nGet API docs at /api';
   }
 }
